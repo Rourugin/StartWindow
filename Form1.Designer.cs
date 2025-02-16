66aa2a4c564fe9841aed520943382413321a7cd5
Rourugin <maks.4min@gmail.com>
2025-02-16 21:48:10 +0300

    Make Programig Set

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,13 +28,261 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.Main_Title = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.plusPS1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.IdePS = new System.Windows.Forms.PictureBox();
+            this.GoogleChromePS = new System.Windows.Forms.PictureBox();
+            this.GoogleChromePSLbl = new System.Windows.Forms.Label();
+            this.DropDownListPS = new System.Windows.Forms.ComboBox();
+            this.UnrealEngine = new System.Windows.Forms.PictureBox();
+            this.plusPS2 = new System.Windows.Forms.Label();
+            this.startupPS = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.IdePS)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.GoogleChromePS)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.UnrealEngine)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Main_Title
+            // 
+            this.Main_Title.Font = new System.Drawing.Font("Microsoft Sans Serif", 55F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Main_Title.ForeColor = System.Drawing.SystemColors.ControlDark;
+            this.Main_Title.Location = new System.Drawing.Point(589, 9);
+            this.Main_Title.Name = "Main_Title";
+            this.Main_Title.Size = new System.Drawing.Size(789, 111);
+            this.Main_Title.TabIndex = 0;
+            this.Main_Title.Text = "Choose startup script";
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlDark;
+            this.label1.Location = new System.Drawing.Point(640, 131);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(648, 35);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Choose startup script for your PC, it will automathic open programs you want";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.startupPS);
+            this.groupBox1.Controls.Add(this.plusPS2);
+            this.groupBox1.Controls.Add(this.UnrealEngine);
+            this.groupBox1.Controls.Add(this.DropDownListPS);
+            this.groupBox1.Controls.Add(this.GoogleChromePSLbl);
+            this.groupBox1.Controls.Add(this.IdePS);
+            this.groupBox1.Controls.Add(this.plusPS1);
+            this.groupBox1.Controls.Add(this.GoogleChromePS);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Location = new System.Drawing.Point(65, 196);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(575, 744);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "1";
+            // 
+            // plusPS1
+            // 
+            this.plusPS1.AutoSize = true;
+            this.plusPS1.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.plusPS1.ForeColor = System.Drawing.SystemColors.ControlDark;
+            this.plusPS1.Location = new System.Drawing.Point(167, 135);
+            this.plusPS1.Name = "plusPS1";
+            this.plusPS1.Size = new System.Drawing.Size(40, 42);
+            this.plusPS1.TabIndex = 2;
+            this.plusPS1.Text = "+";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ControlDark;
+            this.label2.Location = new System.Drawing.Point(145, 30);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(223, 31);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Programming Set";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Location = new System.Drawing.Point(668, 196);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(567, 744);
+            this.groupBox2.TabIndex = 3;
+            this.groupBox2.TabStop = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ControlDark;
+            this.label3.Location = new System.Drawing.Point(220, 30);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(156, 31);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Gaming Set";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.label4);
+            this.groupBox3.Location = new System.Drawing.Point(1265, 196);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(567, 744);
+            this.groupBox3.TabIndex = 4;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "1";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.ForeColor = System.Drawing.SystemColors.ControlDark;
+            this.label4.Location = new System.Drawing.Point(213, 30);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(171, 31);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Common Set";
+            // 
+            // IdePS
+            // 
+            this.IdePS.BackgroundImage = global::StartWindow.Properties.Resources.VS_logo;
+            this.IdePS.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.IdePS.Location = new System.Drawing.Point(213, 88);
+            this.IdePS.Name = "IdePS";
+            this.IdePS.Size = new System.Drawing.Size(155, 140);
+            this.IdePS.TabIndex = 3;
+            this.IdePS.TabStop = false;
+            // 
+            // GoogleChromePS
+            // 
+            this.GoogleChromePS.BackgroundImage = global::StartWindow.Properties.Resources.google_chrome;
+            this.GoogleChromePS.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.GoogleChromePS.InitialImage = ((System.Drawing.Image)(resources.GetObject("GoogleChromePS.InitialImage")));
+            this.GoogleChromePS.Location = new System.Drawing.Point(6, 88);
+            this.GoogleChromePS.Name = "GoogleChromePS";
+            this.GoogleChromePS.Size = new System.Drawing.Size(155, 140);
+            this.GoogleChromePS.TabIndex = 1;
+            this.GoogleChromePS.TabStop = false;
+            // 
+            // GoogleChromePSLbl
+            // 
+            this.GoogleChromePSLbl.AutoSize = true;
+            this.GoogleChromePSLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.GoogleChromePSLbl.ForeColor = System.Drawing.SystemColors.ControlDark;
+            this.GoogleChromePSLbl.Location = new System.Drawing.Point(6, 245);
+            this.GoogleChromePSLbl.Name = "GoogleChromePSLbl";
+            this.GoogleChromePSLbl.Size = new System.Drawing.Size(162, 25);
+            this.GoogleChromePSLbl.TabIndex = 4;
+            this.GoogleChromePSLbl.Text = "Google Chrome";
+            // 
+            // DropDownListPS
+            // 
+            this.DropDownListPS.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.DropDownListPS.FormattingEnabled = true;
+            this.DropDownListPS.Items.AddRange(new object[] {
+            "Visual Studio",
+            "VS + UE5",
+            "VS Code"});
+            this.DropDownListPS.Location = new System.Drawing.Point(307, 249);
+            this.DropDownListPS.MaxDropDownItems = 3;
+            this.DropDownListPS.Name = "DropDownListPS";
+            this.DropDownListPS.Size = new System.Drawing.Size(155, 21);
+            this.DropDownListPS.TabIndex = 3;
+            this.DropDownListPS.Text = "VS + UE5";
+            this.DropDownListPS.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // UnrealEngine
+            // 
+            this.UnrealEngine.BackgroundImage = global::StartWindow.Properties.Resources.ue5;
+            this.UnrealEngine.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.UnrealEngine.Location = new System.Drawing.Point(414, 88);
+            this.UnrealEngine.Name = "UnrealEngine";
+            this.UnrealEngine.Size = new System.Drawing.Size(155, 140);
+            this.UnrealEngine.TabIndex = 5;
+            this.UnrealEngine.TabStop = false;
+            // 
+            // plusPS2
+            // 
+            this.plusPS2.AutoSize = true;
+            this.plusPS2.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.plusPS2.ForeColor = System.Drawing.SystemColors.ControlDark;
+            this.plusPS2.Location = new System.Drawing.Point(374, 135);
+            this.plusPS2.Name = "plusPS2";
+            this.plusPS2.Size = new System.Drawing.Size(40, 42);
+            this.plusPS2.TabIndex = 6;
+            this.plusPS2.Text = "+";
+            // 
+            // startupPS
+            // 
+            this.startupPS.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.startupPS.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.startupPS.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.startupPS.Location = new System.Drawing.Point(196, 668);
+            this.startupPS.Name = "startupPS";
+            this.startupPS.Size = new System.Drawing.Size(205, 45);
+            this.startupPS.TabIndex = 7;
+            this.startupPS.Text = "Startup";
+            this.startupPS.UseVisualStyleBackColor = false;
+            this.startupPS.Click += new System.EventHandler(this.startupPS_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(25)))), ((int)(((byte)(25)))), ((int)(((byte)(25)))));
+            this.ClientSize = new System.Drawing.Size(1904, 1041);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Main_Title);
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.IdePS)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.GoogleChromePS)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.UnrealEngine)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label Main_Title;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label plusPS1;
+        private System.Windows.Forms.PictureBox GoogleChromePS;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.PictureBox IdePS;
+        private System.Windows.Forms.Label GoogleChromePSLbl;
+        private System.Windows.Forms.ComboBox DropDownListPS;
+        private System.Windows.Forms.PictureBox UnrealEngine;
+        private System.Windows.Forms.Label plusPS2;
+        private System.Windows.Forms.Button startupPS;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using StartWindow.Properties;
 
 namespace StartWindow
 {
@@ -15,6 +17,60 @@ namespace StartWindow
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (DropDownListPS.SelectedIndex == 0)
+            {
+                IdePS.BackgroundImage = Resources.VS_logo;
+                GoogleChromePSLbl.Location = new Point(49, 245);
+                GoogleChromePS.Location = new Point(54, 88);
+                plusPS1.Location = new Point(258, 135);
+                IdePS.Location = new Point(355, 88);
+                plusPS2.Visible = false;
+                UnrealEngine.Visible = false;
+            }
+            else if (DropDownListPS.SelectedIndex == 1)
+            {
+                IdePS.BackgroundImage = Resources.VS_logo;
+                GoogleChromePSLbl.Location = new Point(6, 245);
+                GoogleChromePS.Location = new Point(6, 88);
+                plusPS1.Location = new Point(167, 135);
+                IdePS.Location = new Point(213, 88);
+                plusPS2.Location = new Point(374, 135);
+                UnrealEngine.Location = new Point(414, 88);
+                plusPS2.Visible = true;
+                UnrealEngine.Visible = true;
+            }
+            else if (DropDownListPS.SelectedIndex == 2)
+            {
+                IdePS.BackgroundImage = Resources.VSCode_logo;
+                GoogleChromePSLbl.Location = new Point(49, 245);
+                GoogleChromePS.Location = new Point(54, 88);
+                plusPS1.Location = new Point(258, 135);
+                IdePS.Location = new Point(355, 88);
+                plusPS2.Visible = false;
+                UnrealEngine.Visible = false;
+            }
+        }
+
+        private void startupPS_Click(object sender, EventArgs e)
+        {
+            Process.Start("\"C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe\"");
+            if (DropDownListPS.SelectedIndex == 0)
+            {
+                Process.Start("\"C:\\Program Files\\Microsoft Visual Studio\\2022\\Community\\Common7\\IDE\\devenv.exe\"");
+            }
+            else if (DropDownListPS.SelectedIndex == 1)
+            {
+                Process.Start("\"C:\\Program Files\\Epic Games\\UE_5.5\\Engine\\Binaries\\Win64\\UnrealEditor.exe\"");
+                Process.Start("\"C:\\Program Files\\Microsoft Visual Studio\\2022\\Community\\Common7\\IDE\\devenv.exe\"");
+            }
+            else if (DropDownListPS.SelectedIndex == 2)
+            {
+                Process.Start("\"C:\\Users\\maks\\AppData\\Local\\Programs\\Microsoft VS Code\\Code.exe\"");
+            }
         }
     }
 }
